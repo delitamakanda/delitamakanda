32d422c0b9bffc6ad818d40dfc78f5d286e2a24a
Délita Makanda <delitamakanda@users.noreply.github.com>
2020-07-19 22:59:30 +0200

    Update build_readme.py

--- build_readme.py
+++ build_readme.py
@@ -25,11 +25,11 @@ def replace_chunk(content, marker, chunk, inline=False):
     return r.sub(chunk, content)
 
 
-def make_query(after_cursor=None):
+def make_query():
     return """
 query {
   viewer {
-    repositories(first: 5, privacy: PUBLIC, after:AFTER) {
+    repositories(first: 100, privacy: PUBLIC) {
       pageInfo {
         hasNextPage
         endCursor
@@ -50,48 +50,39 @@ query {
     }
   }
 }
-""".replace(
-        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
-    )
+"""
 
 
 def fetch_releases(oauth_token):
     repos = []
     releases = []
     repo_names = set()
-    has_next_page = True
-    after_cursor = None
 
-    while has_next_page:
-        data = client.execute(
-            query=make_query(after_cursor),
-            headers={"Authorization": "Bearer {}".format(oauth_token)},
-        )
-        print()
-        print(json.dumps(data, indent=4))
-        print()
-        for repo in data["data"]["viewer"]["repositories"]["nodes"]:
-            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
-                repos.append(repo)
-                repo_names.add(repo["name"])
-                releases.append(
-                    {
-                        "repo": repo["name"],
-                        "repo_url": repo["url"],
-                        "description": repo["description"],
-                        "release": repo["releases"]["nodes"][0]["name"]
-                        .replace(repo["name"], "")
-                        .strip(),
-                        "published_at": repo["releases"]["nodes"][0][
-                            "publishedAt"
-                        ].split("T")[0],
-                        "url": repo["releases"]["nodes"][0]["url"],
-                    }
-                )
-        has_next_page = data["data"]["viewer"]["repositories"]["pageInfo"][
-            "hasNextPage"
-        ]
-        after_cursor = data["data"]["viewer"]["repositories"]["pageInfo"]["endCursor"]
+    data = client.execute(
+        query=make_query(),
+        headers={"Authorization": "Bearer {}".format(oauth_token)},
+    )
+    print()
+    print(json.dumps(data, indent=4))
+    print()
+    for repo in data["data"]["viewer"]["repositories"]["nodes"]:
+        if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
+            repos.append(repo)
+            repo_names.add(repo["name"])
+            releases.append(
+                {
+                    "repo": repo["name"],
+                    "repo_url": repo["url"],
+                    "description": repo["description"],
+                    "release": repo["releases"]["nodes"][0]["name"]
+                    .replace(repo["name"], "")
+                    .strip(),
+                    "published_at": repo["releases"]["nodes"][0][
+                        "publishedAt"
+                    ].split("T")[0],
+                    "url": repo["releases"]["nodes"][0]["url"],
+                }
+            )
     return releases
 
 
